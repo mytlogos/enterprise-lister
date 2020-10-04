0c0cf40484d3db3aaa851786c07c8ee0e6c9f710
Mytlogos <mytlogos@hotmail.de>
2020-10-04 11:54:56 +0200

    add eslintrc

---
+++ server/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "project": "server/tsconfig.json",
+        "sourceType": "module"
+    }
+};
