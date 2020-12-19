c4ee9a95c53cb0efeab7d954bfe9b1a34ba1510e
Mytlogos <mytlogos@hotmail.de>
2020-12-19 16:17:11 +0100

    feat(openapi): add hook to specify parameter and requestBody properties in separate file
    to correct or specifiy some properties manually, while it does not work automatically

--- src/server/bin/misc/openapi/generate.ts
+++ src/server/bin/misc/openapi/generate.ts
@@ -1,15 +1,129 @@
 import { generateExpressApiObject } from "./exportOpenApi";
 import ts from "typescript";
 import { generateOpenApiSpec, generateWebClient } from "./transformer";
+import { OperationObject, ParameterObject, SchemaObject, RequestBodyObject, OpenApiObject } from "./types";
+import yaml from "js-yaml";
+import fs from "fs/promises";
 
-function GenerateOpenApi(file: string) {
+async function GenerateOpenApi(file: string) {
     const openApiObject = generateExpressApiObject([file], {
         target: ts.ScriptTarget.ES5,
         module: ts.ModuleKind.CommonJS
     });
 
-    generateOpenApiSpec(openApiObject);
-    generateWebClient(openApiObject);
+    await readHook(openApiObject);
+
+    await generateOpenApiSpec(openApiObject);
+    await generateWebClient(openApiObject);
+}
+
+async function readHook(openApiObject: OpenApiObject) {
+    const hook = yaml.load(await fs.readFile("openApiTypeHook.yaml", { encoding: "utf8" }));
+    const methods = ["get", "delete", "post", "put"];
+
+    for (const [path, value] of Object.entries(openApiObject.paths)) {
+        const input: any = hook[path];
+
+        for (const method of methods) {
+            // @ts-expect-error
+            const operation: OperationObject = value[method];
+            const methodInput: any = input && input[method];
+
+            if (operation) {
+                if (operation.parameters && operation.parameters.length) {
+                    const inputParameter = methodInput.parameter || [];
+
+                    operation.parameters.forEach(p => {
+                        const param = p as ParameterObject;
+
+                        const inputParam = inputParameter[param.name];
+
+                        if (!inputParam) {
+                            return;
+                        }
+                        param.schema = inputParam;
+                    });
+                }
+
+                if (operation.requestBody) {
+                    const requestSchema = (operation.requestBody as RequestBodyObject).content["application/json"]?.schema as SchemaObject;
+
+                    if (requestSchema && requestSchema.properties) {
+                        const parameterInput: Record<string, SchemaObject> = methodInput.requestBody || {};
+
+                        for (const [key, schema] of Object.entries(requestSchema.properties)) {
+                            if (!schema || (schema as SchemaObject).type === "any" && parameterInput[key]) {
+                                requestSchema.properties[key] = parameterInput[key];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+async function writeHook(openApiObject: OpenApiObject) {
+    
+    const hookOutput: any = {};
+    const methods = ["get", "delete", "post", "put"];
+
+    for (const [path, value] of Object.entries(openApiObject.paths)) {
+        const output: any = {
+
+        };
+        for (const method of methods) {
+            // @ts-expect-error
+            const operation: OperationObject = value[method];
+            const methodOutput: any = {};
+
+            if (operation) {
+                if (operation.parameters && operation.parameters.length) {
+                    const parameterOutput: Record<string, SchemaObject> = methodOutput.parameter = {};
+
+                    operation.parameters.forEach(p => {
+                        const param = p as ParameterObject;
+
+                        parameterOutput[param.name] = {
+                            ...(param.schema as SchemaObject || {
+                                type: "string"
+                            })
+                        };
+                    });
+                }
+
+                if (operation.requestBody) {
+                    const requestSchema = (operation.requestBody as RequestBodyObject).content["application/json"]?.schema as SchemaObject;
+
+                    if (requestSchema && requestSchema.properties) {
+                        const parameterOutput: Record<string, SchemaObject> = {};
+
+                        for (const [key, schema] of Object.entries(requestSchema.properties)) {
+                            if (!schema || (schema as SchemaObject).type === "any") {
+                                parameterOutput[key] = {
+                                    ...(schema as SchemaObject || {
+                                        type: "string"
+                                    }),
+                                    type: "string",
+                                };
+                            }
+                        }
+                        if (Object.keys(parameterOutput).length) {
+                            methodOutput.requestBody = parameterOutput;
+                        }
+                    }
+                }
+            }
+            if (Object.keys(methodOutput).length) {
+                output[method] = methodOutput;
+            }
+        }
+        if (Object.keys(output).length) {
+            hookOutput[path] = output;
+        }
+    }
+
+    await fs.writeFile("openApiTypeHook.yaml", yaml.dump(hookOutput), { encoding: "utf8" });
 }
 
 GenerateOpenApi("./src/server/bin/api.ts");
