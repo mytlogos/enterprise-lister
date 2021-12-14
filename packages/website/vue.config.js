23b787bc1378b368d5bc2beb23d774ed08f6b800
mytlogos <mytlogos@hotmail.com>
2021-12-14 22:36:48 +0100

    fix: fix pwa not working when offline
    - previously used app.html as root
    - now using the index.html as root works, as pwa requests index.html when offline

--- packages/website/vue.config.js
+++ packages/website/vue.config.js
@@ -4,13 +4,6 @@ const path = require("path");
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  outputDir: "dist",
-  pages: {
-    app: {
-      entry: "src/main.ts",
-      template: "public/index.html",
-    },
-  },
   configureWebpack: {
     devtool: "source-map",
   },
