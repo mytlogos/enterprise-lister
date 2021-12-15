74d741807192a4d4e456c1ea7195bf1c4adb81bb
mytlogos <mytlogos@hotmail.com>
2021-12-15 12:59:56 +0100

    fix(server): restrict index redirect

--- packages/server/bin/app.ts
+++ packages/server/bin/app.ts
@@ -76,9 +76,11 @@ app.use("/doc", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(express.static(path.join(parentDirName, "website", "dist")));
 
+const ignorePaths = ["/api", "/doc", "/js/", "/css/", "/img/"];
+
 app.use((req, res) => {
   // all other GET requests should be the pwa root with path from pwa router
-  if (!req.path.startsWith("/api") && !req.path.startsWith("/doc") && req.method === "GET") {
+  if (req.method === "GET" && ignorePaths.every((ignored) => !req.path.startsWith(ignored))) {
     res.sendFile(path.join(parentDirName, path.join("website", "dist", "index.html")));
   }
 });
