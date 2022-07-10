447303176835085559bbed784239af1041973d05
mytlogos <mytlogos@hotmail.com>
2022-07-10 20:50:22 +0200

    feat(website): initial v2 custom hook

---
+++ packages/website/src/components/customHook/custom-hook-form-v2.vue
@@ -0,0 +1,317 @@
+<template>
+  <div class="container p-4">
+    <div class="row mb-3 align-items-center">
+      <label for="validationCustom01" class="form-label" style="flex: 0.12 0 0%">Hook name</label>
+      <input
+        id="validationCustom01"
+        v-model="name"
+        type="text"
+        class="col form-control"
+        placeholder="Name required"
+        required
+      />
+      <div class="col-3 form-check form-switch ms-2">
+        <input
+          id="hookEnabled"
+          v-model="enabled"
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          :checked="enabled"
+        />
+        <label class="form-check-label" for="hookEnabled">Hook Enabled</label>
+      </div>
+    </div>
+    <div class="form-floating mb-3">
+      <textarea
+        id="floatingTextarea"
+        v-model="comment"
+        class="form-control"
+        placeholder="Leave a comment here"
+      ></textarea>
+      <label for="floatingTextarea">Comments</label>
+    </div>
+    <select v-model="medium" class="form-select mb-3" aria-label="Select Hook Medium">
+      <option value="" disabled selected>Select Medium</option>
+      <option :value="1">Text</option>
+      <option :value="2">Audio</option>
+      <option :value="4">Video</option>
+      <option :value="8">Image</option>
+    </select>
+    <div class="row mb-3 align-items-center">
+      <div class="col">
+        <label for="hookBase" class="form-label">Base URL</label>
+        <input
+          id="hookBase"
+          v-model="baseUrl"
+          type="text"
+          class="form-control"
+          placeholder="Base URL for Links"
+          required
+        />
+      </div>
+      <regex v-model="domain" regex-name="Valid domain regex" />
+    </div>
+    <div>
+      <div>
+        <button class="btn btn-primary me-1" role="button" @click="addToc">Create Toc Scraper</button>
+        <button class="btn btn-primary me-1" role="button" :disabled="!!searchConfig" @click="addSearch">
+          Create Search Scraper
+        </button>
+        <button class="btn btn-primary me-1" role="button" :disabled="!!newsConfig" @click="addNews">
+          Create News Scraper
+        </button>
+        <button class="btn btn-primary" role="button" :disabled="!!downloadConfig" @click="addDownload">
+          Create Download Scraper
+        </button>
+      </div>
+      <template v-for="(item, index) in tocConfig" :key="index">
+        <scraper-config
+          v-model="tocConfig[index]"
+          :id-prefix="'tocConfig' + index + '-'"
+          :name="'TocConfig ' + index"
+          @delete="remove(tocConfig, index)"
+        />
+      </template>
+      <template v-if="searchConfig">
+        <scraper-config
+          v-model="searchConfig"
+          id-prefix="searchConfig"
+          name="SearchConfig"
+          @delete="removeConfig('searchConfig')"
+        >
+          <div class="col">
+            <label for="newsUrl" class="form-label">Search URL</label>
+            <input
+              id="newsUrl"
+              v-model="searchConfig.searchUrl"
+              type="text"
+              class="form-control"
+              placeholder="https://iamanewspage.com/"
+              required
+            />
+          </div>
+        </scraper-config>
+      </template>
+      <template v-if="newsConfig">
+        <scraper-config
+          v-model="newsConfig"
+          id-prefix="newsConfig"
+          name="NewsConfig"
+          @delete="removeConfig('newsConfig')"
+        >
+          <div class="col">
+            <label for="newsUrl" class="form-label">News URL</label>
+            <input
+              id="newsUrl"
+              v-model="newsConfig.newsUrl"
+              type="text"
+              class="form-control"
+              placeholder="https://iamanewspage.com/"
+              required
+            />
+          </div>
+        </scraper-config>
+      </template>
+      <template v-if="downloadConfig">
+        <scraper-config
+          v-model="downloadConfig"
+          id-prefix="downloadConfig"
+          name="DownloadConfig"
+          @delete="removeConfig('downloadConfig')"
+        />
+      </template>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { CustomHook, HookState } from "enterprise-core/dist/types";
+import { HookConfig, Selector as SelectorType, JsonRegex } from "enterprise-scraper/dist/externals/custom/types";
+import { defineComponent, PropType } from "vue";
+import ScraperConfig from "./scraper-config.vue";
+import Regex from "./regex.vue";
+import "bootstrap/js/dist/collapse";
+import { toArray, deepEqual, Logger } from "../../init";
+import { MediaType } from "../../siteTypes";
+
+export default defineComponent({
+  name: "CustomHookForm",
+  components: {
+    ScraperConfig,
+    Regex,
+  },
+  props: {
+    hook: {
+      type: Object as PropType<CustomHook>,
+      required: true,
+    },
+    config: {
+      type: Object as PropType<HookConfig>,
+      required: true,
+    },
+  },
+  emits: ["update:hook", "update:config"],
+  data() {
+    return {
+      name: this.hook.name,
+      enabled: this.hook.hookState ? this.hook.hookState === HookState.ENABLED : true,
+      comment: this.hook.comment,
+      baseUrl: this.config.base,
+      medium: MediaType.TEXT,
+      domain: this.config.domain || ({ pattern: "", flags: "" } as JsonRegex),
+      tocConfig: toArray(this.config.toc),
+      searchConfig: this.config.search && { ...this.config.search },
+      newsConfig: this.config.news && { ...this.config.news },
+      downloadConfig: this.config.download && { ...this.config.download },
+      logger: new Logger("custom-hook-form"),
+    };
+  },
+  computed: {
+    hookModel(): CustomHook {
+      return {
+        ...this.hook,
+        name: this.name,
+        comment: this.comment,
+        hookState: this.enabled ? HookState.ENABLED : HookState.DISABLED,
+      };
+    },
+    configModel(): HookConfig {
+      return {
+        name: this.name,
+        domain: this.domain,
+        base: this.baseUrl,
+        medium: Number(this.medium),
+        download: this.downloadConfig,
+        news: this.newsConfig,
+        toc: this.tocConfig,
+        search: this.searchConfig,
+      };
+    },
+  },
+  watch: {
+    hookModel: {
+      handler(newValue: CustomHook) {
+        if (deepEqual(newValue, this.hook)) {
+          this.logger.info("Did not update hook");
+        } else {
+          this.logger.info("Updated hook");
+          this.$emit("update:hook", newValue);
+        }
+      },
+      deep: true,
+    },
+    configModel: {
+      handler(newValue: HookConfig) {
+        if (deepEqual(newValue, this.hook)) {
+          this.logger.info("Did not update config");
+        } else {
+          this.logger.info("Updated config");
+          this.$emit("update:config", newValue);
+        }
+      },
+      deep: true,
+    },
+    hook: {
+      handler(newValue: CustomHook) {
+        if (newValue.comment !== this.comment) {
+          this.comment = newValue.comment;
+        }
+        const enabled = newValue.hookState === HookState.ENABLED;
+        if (enabled !== this.enabled) {
+          this.enabled = enabled;
+        }
+        if (newValue.name !== this.name) {
+          this.name = newValue.name;
+        }
+      },
+      deep: true,
+    },
+    config: {
+      handler(newValue: HookConfig) {
+        if (newValue.base !== this.baseUrl) {
+          this.baseUrl = newValue.base;
+        }
+        if (newValue.medium != this.medium) {
+          this.medium = newValue.medium;
+        }
+        if (newValue.name !== this.name) {
+          this.name = newValue.name;
+        }
+        if (!deepEqual(newValue.domain, this.domain)) {
+          this.domain = newValue.domain;
+        }
+        if (!deepEqual(newValue.news, this.newsConfig)) {
+          this.newsConfig = newValue.news;
+        }
+        if (!deepEqual(newValue.toc, this.tocConfig)) {
+          this.tocConfig = toArray(newValue.toc);
+        }
+        if (!deepEqual(newValue.download, this.downloadConfig)) {
+          this.downloadConfig = newValue.download;
+        }
+        if (!deepEqual(newValue.search, this.searchConfig)) {
+          this.searchConfig = newValue.search;
+        }
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    remove(array: any[], index: number) {
+      array.splice(index, 1);
+    },
+    removeConfig(prop: "searchConfig" | "newsConfig" | "downloadConfig") {
+      this[prop] = undefined;
+    },
+    addToc() {
+      this.tocConfig.push({
+        selector: [{ selector: "" }],
+        prefix: undefined,
+        base: undefined,
+        request: undefined,
+      });
+    },
+    addNews() {
+      this.newsConfig = {
+        newsUrl: "",
+        selector: [{ selector: "" }],
+      };
+    },
+    addSearch() {
+      this.searchConfig = {
+        searchUrl: "",
+        base: undefined,
+        request: undefined,
+        selector: [{ selector: "" }],
+      };
+    },
+    addDownload() {
+      this.downloadConfig = {
+        prefix: undefined,
+        base: undefined,
+        request: undefined,
+        selector: [{ selector: "" }],
+      };
+    },
+    addSelector(config: any) {
+      if (!Array.isArray(config.selector)) {
+        config.selector = toArray(config.selector);
+      }
+      config.selector.push({
+        selector: "",
+      } as SelectorType<any>);
+    },
+  },
+});
+</script>
+<style scoped>
+.card[aria-expanded="true"] {
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+.card + * > .card {
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  border-top: 0;
+}
+</style>
---
+++ packages/website/src/views/CustomHookViewV2.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="w-100">
+    <div class="row">
+      <div class="col text-end">
+        <button class="btn btn-success" @click="save">Save</button>
+      </div>
+    </div>
+    <div v-if="createResult === 'success'" class="alert alert-success" role="alert">
+      Successfully saved CustomHook {{ value.name }}
+    </div>
+    <div v-else-if="createResult === 'failed'" class="alert alert-danger" role="alert">
+      Failed saving CustomHook {{ value.name }}
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="row g-3 align-items-center">
+          <div class="col-auto">
+            <label for="param" class="form-label">Parameter</label>
+          </div>
+          <div class="col-auto">
+            <input id="param" v-model="param" class="form-control" name="param" type="text" />
+          </div>
+        </div>
+      </div>
+      <div class="col text-end">
+        <button class="btn btn-primary me-1" :disabled="!value['news']" @click="testHook('news')">Test News</button>
+        <button
+          class="btn btn-primary me-1"
+          :disabled="!value['toc'] || (Array.isArray(value.toc) && !value.toc.length)"
+          @click="testHook('toc')"
+        >
+          Test ToC
+        </button>
+        <button class="btn btn-primary me-1" :disabled="!value['search']" @click="testHook('search')">
+          Test Search
+        </button>
+        <button class="btn btn-primary" :disabled="!value['download']" @click="testHook('download')">
+          Test Download
+        </button>
+      </div>
+    </div>
+    <div>Result:</div>
+    <div v-if="loading || result" class="alert alert-info" role="alert">
+      <div v-if="loading" class="d-flex align-items-center">
+        <strong>Processing...</strong>
+        <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+      </div>
+      <span v-else-if="result" style="white-space: pre">{{ result }}</span>
+    </div>
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item" role="presentation">
+        <button
+          data-bs-toggle="tab"
+          class="nav-link"
+          :class="{ active: isActiveTab('form') }"
+          data-bs-target="#hook-form"
+          type="button"
+          role="tab"
+          @click.prevent="setActiveTab('form')"
+        >
+          Form
+        </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button
+          data-bs-toggle="tab"
+          class="nav-link"
+          :class="{ active: isActiveTab('editor') }"
+          data-bs-target="#hook-editor"
+          type="button"
+          role="tab"
+          @click.prevent="setActiveTab('editor')"
+        >
+          JSON Editor
+        </button>
+      </li>
+    </ul>
+    <custom-hook-form v-model:hook="hook" v-model:config="value" />
+  </div>
+</template>
+
+<script lang="ts">
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+// @ts-expect-error
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-json";
+import "prismjs/themes/prism-twilight.css"; // import syntax highlighting styles
+import type { HookConfig, JsonRegex } from "enterprise-scraper/dist/externals/custom/types";
+import { HttpClient } from "../Httpclient";
+import { defineComponent } from "vue";
+import { HookState } from "../siteTypes";
+import { CustomHook } from "enterprise-core/dist/types";
+import CustomHookForm from "../components/customHook/custom-hook-form-v2.vue";
+import { clone, deepEqual, Logger } from "../init";
+
+interface Data {
+  code: string;
+  invalid: string;
+  param: string;
+  result: string;
+  loading: boolean;
+  createResult?: "success" | "failed";
+  value: Partial<HookConfig>;
+  hook: Partial<CustomHook>;
+  activeTab: "form" | "editor";
+  logger: Logger;
+}
+
+interface BasicSelector {
+  regex?: JsonRegex;
+  children: BasicSelector[];
+}
+
+export default defineComponent({
+  components: {
+    CustomHookForm,
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data(): Data {
+    return {
+      loading: false,
+      logger: new Logger("CustomHookView"),
+      code: "",
+      invalid: "",
+      param: "",
+      result: "",
+      value: {},
+      hook: {
+        id: 0,
+        name: "",
+        comment: "",
+        hookState: HookState.ENABLED,
+        state: "",
+      },
+      createResult: undefined,
+      activeTab: "form",
+    };
+  },
+  watch: {
+    code(newValue: string) {
+      try {
+        // TODO: debounce this
+        this.setConfig(JSON.parse(newValue));
+        this.invalid = "";
+      } catch (e) {
+        if (e && typeof e === "object" && "message" in e) {
+          this.invalid = (e as Record<string, any>).message;
+        } else {
+          this.invalid = e + "";
+        }
+      }
+    },
+    value(newValue: any) {
+      this.code = JSON.stringify(newValue, undefined, 2);
+    },
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    setConfig(value: Partial<HookConfig>) {
+      if (deepEqual(value, this.value)) {
+        this.logger.info("No config update required");
+        return;
+      }
+      this.logger.info("Updated HookConfig");
+      this.value = value;
+    },
+    validateSelector<T extends BasicSelector>(value: T) {
+      if (!value.regex) {
+        value.regex = { pattern: "", flags: "" };
+      }
+      if (value.children) {
+        value.children.forEach((child: BasicSelector) => this.validateSelector(child));
+      }
+    },
+    isActiveTab(tab: Data["activeTab"]) {
+      return this.activeTab === tab;
+    },
+    setActiveTab(tab: Data["activeTab"]) {
+      this.activeTab = tab;
+    },
+    highlighter(code: string) {
+      return highlight(code, languages.json); // languages.<insert language> to return html with markup
+    },
+    cleanSelector(value: any) {
+      if (!value) {
+        return;
+      }
+      if (typeof value.regex === "object" && !value.regex.pattern && !value.regex.flags) {
+        delete value.regex;
+
+        for (const transfer of value.transfers || []) {
+          if (!transfer.extract) {
+            delete transfer.extract;
+          }
+        }
+        for (const variable of value.variables || []) {
+          if (!variable.value) {
+            delete variable.value;
+          }
+        }
+      }
+      for (const child of value.children || []) {
+        this.cleanSelector(child);
+      }
+    },
+    cleanEmptyObject(value: any) {
+      if (typeof value !== "object") {
+        return;
+      }
+      if (Array.isArray(value)) {
+        for (let index = 0; index < value.length; index++) {
+          const keyValue = value[index];
+
+          if (typeof keyValue === "object" && keyValue) {
+            this.cleanEmptyObject(keyValue);
+
+            if (!Object.keys(keyValue).length) {
+              value.splice(index, 1);
+              index--;
+            }
+          } else if (typeof keyValue === "string" && !keyValue) {
+            value.splice(index, 1);
+            index--;
+          }
+        }
+        return;
+      }
+      for (const [key, keyValue] of Object.entries(value)) {
+        if (typeof keyValue === "object" && keyValue) {
+          this.cleanEmptyObject(keyValue);
+
+          if (!Object.keys(keyValue).length) {
+            delete value[key];
+          }
+        } else if (typeof keyValue === "string" && !keyValue) {
+          delete value[key];
+        }
+      }
+    },
+    clean(value: any) {
+      if (!value) {
+        return;
+      }
+      if (Array.isArray(value.selector)) {
+        value.selector.forEach((selector: any) => this.cleanSelector(selector));
+      } else {
+        this.cleanSelector(value.selector);
+      }
+      this.cleanEmptyObject(value);
+    },
+    testHook(hookKey: keyof HookConfig) {
+      if (this.loading) {
+        return;
+      }
+      this.loading = true;
+
+      const hookConfig = clone(this.value);
+      this.clean(hookConfig.news);
+      this.clean(hookConfig.toc);
+      this.clean(hookConfig.download);
+      this.clean(hookConfig.search);
+
+      HttpClient.testHook({
+        config: hookConfig as any,
+        key: hookKey,
+        param: this.param,
+      })
+        .then((value) => (this.result = JSON.stringify(value)))
+        .catch((value) => {
+          this.result = (value.message ? value.message + "\n" : "") + JSON.stringify(value);
+        })
+        .finally(() => (this.loading = false));
+    },
+
+    load() {
+      if (this.id) {
+        // simple but stupid way to clone the hook, firefox went off alone in 94 and introduced "structuredClone" (with Node 17 support at this time)
+        // when there is wider support, maybe use that, else if lodash is ever used use that cloneDeep
+        this.hook = clone(this.$store.state.hooks.hooks[this.id]);
+        this.code = this.hook.state || "";
+      }
+    },
+
+    save() {
+      if (this.value.name && !this.hook.name) {
+        this.hook.name = this.value.name;
+      }
+
+      if (!this.hook.name) {
+        console.error("No name defined!");
+        return;
+      }
+
+      this.hook.state = this.code;
+
+      const action = this.hook.id ? "updateHook" : "createHook";
+
+      this.$store
+        .dispatch(action, this.hook)
+        .then((value: CustomHook) => {
+          console.log(value);
+          this.hook = value;
+          this.code = value.state;
+          this.createResult = "success";
+        })
+        .catch((value: any) => {
+          console.log(value);
+          this.createResult = "failed";
+        });
+    },
+  },
+});
+</script>
