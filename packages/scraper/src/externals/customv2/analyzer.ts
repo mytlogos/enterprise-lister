6f24dee37280b6828f2c4b8e0dc9483f3c38f178
mytlogos <mytlogos@hotmail.com>
2022-07-19 15:40:53 +0200

    feat(scraper): improve analyzer

--- packages/scraper/src/externals/customv2/analyzer.ts
+++ packages/scraper/src/externals/customv2/analyzer.ts
@@ -1,13 +1,49 @@
 import { JsonRegex } from "./types";
 import { finder } from "./css-selector";
 
+interface Scorer {
+  propertyKey: string;
+}
+
+interface TagScorer extends Scorer {
+  tagName: string;
+}
+
+interface TextScorer extends Scorer {
+  pattern: RegExp;
+}
+
+interface AttributeScorer extends Scorer {
+  pattern: RegExp;
+  attr: RegExp;
+}
+
+interface DescendantScorer extends Scorer {
+  descendantOf: string;
+}
+
+type ArrayScorer = Scorer;
+
+interface GroupScorer {
+  group: string[];
+}
+
 interface PropertyConfig {
   require?: {
     tag?: string;
-    content?: {
-      type: "text" | "attribute";
-      pattern?: JsonRegex;
-    };
+    content?:
+      | {
+          type: "text";
+          pattern: JsonRegex;
+          minLength?: number;
+          maxLength?: number;
+        }
+      | {
+          type: "attribute";
+          attr?: string; // either attr or pattern or both, none is illegal
+          pattern?: JsonRegex;
+        };
+    descendantOf?: string;
   };
   properties?: Record<string, PropertyConfig>;
   array?: boolean;
@@ -22,6 +58,43 @@ interface PropertyScore {
    * The total score
    */
   score: number;
+
+  /**
+   * Total score of the node
+   */
+  nodeScore: number;
+
+  /**
+   * Total score of the node
+   */
+  descendantScore: number[];
+
+  /**
+   * Generalscore dependant on the node attributes.
+   * e.g. banner and comments should never be really used.
+   */
+  generalScore: number;
+
+  /**
+   * Score based on text pattern.
+   */
+  patternScore: number;
+
+  /**
+   * Score based on text pattern.
+   */
+  tagScore: number;
+
+  /**
+   * Score scaled on how many children with score a node has.
+   */
+  childrenScore: number;
+
+  /**
+   *
+   */
+  siblingScore: number;
+
   /**
    * Number of children with such a PropertyScore per descendentLevel
    */
@@ -75,7 +148,58 @@ export class ScrapeAnalyzer {
       node.analyzer = {};
     }
     if (!node.analyzer[key]) {
-      node.analyzer[key] = { score: 0, levels: [] };
+      node.analyzer[key] = {
+        score: 0,
+        levels: [0],
+        generalScore: 0,
+        descendantScore: [0],
+        nodeScore: 0,
+        patternScore: 0,
+        tagScore: 0,
+        childrenScore: 0,
+        siblingScore: 0,
+      } as PropertyScore;
+    }
+    return node.analyzer[key];
+  }
+
+  private scoreProperty(node: HTMLElement, propertyKey: string, generalScore: number, value: PropertyConfig) {
+    if (generalScore) {
+      this.initNode(node, propertyKey).generalScore = generalScore;
+    }
+    const require = value.require;
+
+    if (require) {
+      if (require.tag && require.tag.toLowerCase() === node.tagName.toLowerCase()) {
+        this.initNode(node, propertyKey).tagScore += 5;
+      }
+      if (require.content) {
+        if (require.content.type === "text") {
+          const textValue = node.textContent || "";
+
+          if (require.content.pattern) {
+            const pattern = new RegExp(require.content.pattern.pattern, require.content.pattern.flags);
+            const notTooBig = textValue.length <= (require.content.maxLength || Number.POSITIVE_INFINITY);
+            const notTooSmall = textValue.length >= (require.content.minLength || 0);
+
+            if (notTooBig && notTooSmall && pattern.test(textValue)) {
+              this.initNode(node, propertyKey).patternScore += 5;
+            } else {
+              this.initNode(node, propertyKey).patternScore -= 5;
+            }
+          }
+        } else if (require.content.type === "attribute" && require.content.attr && require.content.pattern) {
+          const textValue = node.getAttribute(require.content.attr) || "";
+
+          const pattern = new RegExp(require.content.pattern.pattern, require.content.pattern.flags);
+
+          if (pattern.test(textValue)) {
+            this.initNode(node, propertyKey).patternScore += 5;
+          } else {
+            this.initNode(node, propertyKey).patternScore -= 5;
+          }
+        }
+      }
     }
   }
 
@@ -107,33 +231,51 @@ export class ScrapeAnalyzer {
     for (const [key, value] of Object.entries(properties)) {
       const propertyKey = parentKey ? parentKey + "." + key : key;
 
+      // add score only once
       if (!node.analyzer || !node.analyzer[propertyKey]) {
-        if (value.require?.tag && value.require.tag.toLowerCase() === node.tagName.toLowerCase()) {
-          this.initNode(node, propertyKey);
-          node.analyzer[propertyKey].score += 5 + generalScore;
-        }
-        if (value.require?.content) {
-          if (value.require.content.pattern) {
-            const pattern = new RegExp(value.require.content.pattern.pattern, value.require.content.pattern.flags);
-
-            if (value.require.content.type === "text") {
-              const textValue = node.textContent || "";
-
-              if (textValue.length < 300 && pattern.test(textValue)) {
-                this.initNode(node, propertyKey);
-                node.analyzer[propertyKey].score += 5 + generalScore;
-              }
-            }
-          }
-        }
-      }
-
-      if (node.analyzer && candidates[candidates.length - 1] !== node) {
-        candidates.push(node);
+        this.scoreProperty(node, propertyKey, generalScore, value);
       }
       if (value.properties) {
         this.scoreNode(node, candidates, value.properties, propertyKey);
       }
+    }
+  }
+
+  private propertyMap = new Map<string, PropertyConfig>();
+
+  private getConfigProperty(propertyKey: string, config: Config) {
+    const mapValue = this.propertyMap.get(propertyKey);
+
+    if (mapValue) {
+      return mapValue;
+    }
+    let properties = config.properties;
+    let propertyConfig = null;
+
+    const keyParts = propertyKey.split(".");
+
+    for (let index = 0; index < keyParts.length; index++) {
+      const key = keyParts[index];
+      const value = properties[key];
+
+      if (value) {
+        propertyConfig = value;
+
+        if (value.properties) {
+          properties = value.properties;
+        } else if (index + 1 < keyParts.length) {
+          throw Error(`did not expect another key: '${keyParts[index + 1]}' of ${propertyKey}`);
+        }
+      } else {
+        throw Error(`unknown key part: '${key}' of key '${propertyKey}'`);
+      }
+    }
+
+    if (propertyConfig) {
+      this.propertyMap.set(propertyKey, propertyConfig);
+      return propertyConfig;
+    } else {
+      throw Error("could not find PropertyConfig for " + propertyKey);
     }
   }
 
@@ -148,15 +290,9 @@ export class ScrapeAnalyzer {
       this.visit(child as HTMLElement, candidates, config);
 
       if (child.analyzer) {
-        if (!node.analyzer) {
-          node.analyzer = {};
-        }
         for (const [key, value] of Object.entries(child.analyzer)) {
-          let nodeValue = node.analyzer[key];
-
-          if (!nodeValue) {
-            node.analyzer[key] = nodeValue = { score: 0, levels: [0] };
-          }
+          this.initNode(node, key);
+          const nodeValue = node.analyzer[key];
           nodeValue.levels[0]++;
 
           for (let level = 0; level < value.levels.length; level++) {
@@ -168,20 +304,51 @@ export class ScrapeAnalyzer {
               nodeValue.levels[level + 1] += levelCount;
             }
           }
-          nodeValue.score += value.score / 2;
+          for (let level = 0; level < value.descendantScore.length; level++) {
+            const score = value.descendantScore[level];
+
+            if (level + 1 >= nodeValue.descendantScore.length) {
+              nodeValue.descendantScore.push(score);
+            } else {
+              nodeValue.descendantScore[level + 1] += score;
+            }
+          }
+          nodeValue.descendantScore[0] += value.nodeScore;
         }
       }
     }
 
+    for (let index = 0; index < node.children.length; index++) {
+      const child = node.children[index];
+
+      if (child.analyzer) {
+        // siblingScore?
+        // descendantOf?
+      }
+    }
     this.scoreNode(node, candidates, config.properties);
 
     if (node.analyzer) {
-      Object.values(node.analyzer).forEach((value) => {
-        const count = value.levels[0];
+      candidates.push(node);
+      Object.entries(node.analyzer).forEach(([key, value]) => {
+        const propertyConfig = this.getConfigProperty(key, config);
 
-        if (count) {
-          value.score += 0.5 * count;
+        // only count children for score if property expects an array
+        if (propertyConfig.array) {
+          const count = value.levels[0];
+
+          if (count) {
+            value.childrenScore += 0.5 * count;
+          }
         }
+
+        value.nodeScore = value.generalScore + value.patternScore + value.tagScore + value.childrenScore;
+        value.score =
+          value.descendantScore.reduce(
+            // score per descendant level (one- not zero-based), capped at 3
+            (previous, current, index) => previous + current / (index + 1),
+            0,
+          ) + value.nodeScore;
       });
     }
   }
@@ -204,6 +371,7 @@ export class ScrapeAnalyzer {
   }
 
   public parse(config: Config) {
+    this.propertyMap = new Map<string, PropertyConfig>();
     const result = {} as any;
     this.log("**** grabArticle ****");
     const page = this._doc.body;
@@ -283,6 +451,48 @@ export class ScrapeAnalyzer {
       })
       .slice(-20) // print the 20 best only
       .forEach((value) => console.log(value.analyzer, value.tagName, finder(value)));
+
+    this.visualizeCandidates(candidates);
     return result;
+  }
+
+  private visualizeCandidates(candidates: HTMLElement[]) {
+    const classes = new Set<string>();
+    candidates.forEach((node) => {
+      Object.entries(node.analyzer).forEach(([key, value]) => {
+        if (value.patternScore || value.tagScore) {
+          const keyClass = "analyzer-" + key.replaceAll(".", "_");
+          classes.add(keyClass);
+          node.classList.add(keyClass);
+        }
+      });
+    });
+    const style = this._doc.createElement("style");
+    const overlay = this._doc.createElement("div");
+    this._doc.body.insertBefore(overlay, this._doc.body.firstElementChild);
+    this._doc.head.appendChild(style);
+    const colors = ["aqua", "bisque", "cadetblue", "coral", "orangered", "plum", "yellow"];
+    let css = "";
+    let content = "";
+    [...classes].forEach((value, index) => {
+      css += `.${value} {
+      border: solid 10px ${colors[index]} !important;
+  }
+  `;
+      content += `<div class="${value}">${value}</div>`;
+    });
+
+    overlay.classList.add("analyzer-overlay");
+    overlay.innerHTML = `${content}`;
+    css += `\n.analyzer-overlay {
+      position: sticky;
+      top: 50px;
+      left: 50px;
+      border: solid 10px black !important;
+      z-index: 100;
+      background: white;
+      max-width: 300px;
+  }`;
+    style.appendChild(this._doc.createTextNode(css));
   }
 }
